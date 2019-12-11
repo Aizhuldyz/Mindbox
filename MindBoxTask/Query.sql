1862504e41a666a269114b11fe14d6381f00ba70
aizhuldyz <aigabbass90@gmail.com>
2019-12-12 00:39:23 +0600

    sql query

---
+++ MindBoxTask/Query.sql
@@ -0,0 +1,10 @@
+﻿select a.Name As ArticleName, ATN.TagName
+from dbo.Article as A
+left join 
+(
+    select AT.ArticleId, T.Name as TagName
+    from dbo.ArtcileTag as AT 
+    inner join dbo.Tag as T on AT.TagId = T.Id
+) ATN on A.ID = ATN.ArticleId
+
+
